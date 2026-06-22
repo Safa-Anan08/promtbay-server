9b6e2eca606c74c5d3d0d8c8636bd20493e59215
Safa-Anan08 <safaspractice@gmail.com>
2026-06-22 23:29:04 +0600

    optional auth

---
+++ src/middleware/optionalAuth.js
@@ -0,0 +1,49 @@
+const jwt =
+require(
+"jsonwebtoken"
+);
+
+const optionalAuth =
+(
+req,
+res,
+next
+)=>{
+
+try{
+
+const token =
+req.cookies.token;
+
+if(
+token
+){
+
+const decoded =
+jwt.verify(
+
+token,
+
+process.env.JWT_SECRET
+
+);
+
+req.user =
+decoded;
+
+}
+
+next();
+
+}
+
+catch{
+
+next();
+
+}
+
+};
+
+module.exports =
+optionalAuth;
