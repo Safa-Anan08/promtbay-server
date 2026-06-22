cb6164e698894f3571a4b178a2fa3e50a7348a35
Safa-Anan08 <safaspractice@gmail.com>
2026-06-22 23:35:25 +0600

    upload

---
+++ src/middleware/roleMiddleware.js
@@ -0,0 +1,12 @@
+const recruiterOnly = (req, res, next) => {
+  if (req.user.role !== "recruiter") {
+    return res.status(403).json({
+      success: false,
+      message: "Recruiter access only",
+    });
+  }
+
+  next();
+};
+
+module.exports = recruiterOnly;
