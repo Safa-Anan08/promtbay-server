d0cf3ae0182fb484ffeb374ae4b8eb598b692d49
Safa-Anan08 <safaspractice@gmail.com>
2026-06-22 23:48:24 +0600

    bookmark routes added

---
+++ src/routes/bookmarkRoutes.js
@@ -0,0 +1,15 @@
+const router =require("express").Router();
+const verifyToken =require("../middleware/verifyToken");
+const {toggleBookmark,getBookmarks,}=require("../controllers/bookmarkController");
+    router.post(
+    "/:id",
+     verifyToken,
+     toggleBookmark
+        );
+ 
+    router.get("/mine",
+        verifyToken,
+         getBookmarks
+);
+
+module.exports = router;
